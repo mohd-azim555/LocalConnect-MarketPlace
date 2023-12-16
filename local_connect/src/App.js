12f9fc2f4788216efccbf9a21b0ac4c12956ddd6
Azim <moazim555@gmail.com>
2023-12-17 00:00:04 +0530

    navbar

--- local_connect/src/App.js
+++ local_connect/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import NavBar from './Components/Navigation /NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <h1>Local Connect Market Place</h1>
     </div>
   );
